af6efe8eef3a79e1bb60dc9e9f1088f261aa5a07
Dom <dcomte@gmail.com>
2017-11-27 16:36:33 +0100

    fixing IFS
    changed IFS initialisation to put a tab in it without using one in the script file.
    fixed spaces in variable name on line 28

--- scan_spam.sh
+++ scan_spam.sh
@@ -20,17 +20,16 @@ if [ -f "${file}" ]; then
   while read -r line; do
     # ignore lines commented with #
     [[ "${line}" =~ ^#.*$ ]] && continue
-    IFS='       ' read -r -a account <<< "${line}"
+		IFS=$'\t' read -r -a account <<< "${line}"
     # to debug parsing of input file, uncomment next line
     #printf ">[%s]\n" "${account[@]}"
     IFS=${OLD_IFS}
     logger "scanning ${account[1]}/${account[4]}"
     /usr/local/bin/isbg.py --noninteractive --flag \
-      --imaphost ${account[0]} --imapuser "${account[1]}"  --imappasswd "${accou  nt[2]}" \
+      --imaphost ${account[0]} --imapuser "${account[1]}"  --imappasswd "${account[2]}" \
       --spaminbox "${account[3]}" \
       --imapinbox "${account[4]}" 2>&1 | logger
     logger "scanning of ${account[1]}/${account[4]} done"
   done < "$file"
   imapfilter | logger
 fi
-cleanup
