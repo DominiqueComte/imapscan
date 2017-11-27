6dc25b4fdc5d30d9db910b76c9d47ef6335b8baa
Dom <dcomte@gmail.com>
2017-11-27 18:29:18 +0100

    rerun isbg and imapfilter until success

--- learn_spam.sh
+++ learn_spam.sh
@@ -17,22 +17,29 @@ trap cleanup EXIT
 touch "${LOCKFILE}"
 STARTED=1
 set -f
+set -o pipefail
 OLD_IFS=${IFS}
 file="${HOME}/accounts/imap_accounts_learn.txt"
 if [ -f ${file} ]; then
-  while read -r line; do
+  while read -r line
+  do
     # ignore lines commented with #
     [[ "${line}" =~ ^#.*$ ]] && continue
-    IFS='	' read -r -a account <<< "${line}"
+    IFS=$'\t' read -r -a account <<< "${line}"
     # to debug parsing of input file, uncomment next line
     #printf ">[%s]\n" "${account[@]}"
     IFS=${OLD_IFS}
-    logger "learning ${account[1]}/${account[4]}"
-    /usr/local/bin/isbg.py --noninteractive \
-      --imaphost ${account[0]} --imapuser ${account[1]}  --imappasswd ${account[2]} \
-      --learnspambox ${account[3]} \
-      --learnhambox ${account[4]} \
-      --teachonly 2>&1 | logger
-    logger "finished learning from ${account[1]}/${account[4]}"
+    {
+      echo "learning ${account[1]}/${account[4]}"
+      until /usr/local/bin/isbg.py --noninteractive \
+        --imaphost ${account[0]} --imapuser ${account[1]}  --imappasswd ${account[2]} \
+        --learnspambox ${account[3]} \
+        --learnhambox ${account[4]} \
+        --teachonly
+      do
+        (>&2 echo "isbg failed, retrying...")
+      done
+      echo "finished learning from ${account[1]}/${account[4]}"
+    } 2>&1 | logger
   done < "$file"
 fi
