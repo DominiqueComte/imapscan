d8e920a1d3b3b838dd480be8b8379906441006b3
Dominique <dcomte@gmail.com>
2017-12-07 11:35:39 +0100

    added run_example script

---
+++ run_example.cmd
@@ -0,0 +1,5 @@
+setlocal
+set IMAGE_VERSION=0.10
+set CONTAINER_VOLUMES_DIR=d:/docker_volumes/imapscan
+
+docker run -d --name imapscan_%IMAGE_VERSION% -v %CONTAINER_VOLUMES_DIR%/spamassassin:/var/spamassassin -v %CONTAINER_VOLUMES_DIR%/imapfilter:/root/.imapfilter -v %CONTAINER_VOLUMES_DIR%/accounts:/root/accounts domcomte/imapscan:%IMAGE_VERSION%
